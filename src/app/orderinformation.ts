76a9051a29d862fee6966aa48424285b27a1b92b
Wolyaon <jarrick_pang@hotmail.com>
2025-04-07 22:34:13 -0700

    Adding a new interface for orders, updating order page to also take emails. small aesthetic changes.

---
+++ src/app/orderinformation.ts
@@ -0,0 +1,10 @@
+export interface OrderInformation {
+     pickupdate: string;
+     name: string;
+     lactose: string;
+     cakeName: string;
+     cakeSize: string;
+     email: string;
+     message: string;
+     allergies: string;
+}
--- src/app/webservices.ts
+++ src/app/webservices.ts
@@ -2,33 +2,54 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CakeInformation } from './cakeinformation';
 import { Observable } from 'rxjs';
+import { OrderInformation } from './orderinformation';
 
 @Injectable({
     providedIn: 'root'
 })
 export class WebServices{
     private apiURL = "http://127.0.0.1:8000";
+    cakes?: Observable<CakeInformation[]>;
+    tarts?: Observable<CakeInformation[]>;
+    others?: Observable<CakeInformation[]>;
+    all?: Observable<CakeInformation[]>;
 
     constructor(private http: HttpClient) {}
-    
+     
     public get_cakes(): Observable<any>{
-        return this.http.get<CakeInformation[]>(this.apiURL+"/cakes")
+        if (this.cakes == undefined){
+            this.cakes = this.http.get<CakeInformation[]>(this.apiURL+"/cakes")
+        }
+        return this.cakes
     }
 
     public get_tarts(): Observable<any>{
-        return this.http.get<CakeInformation[]>(this.apiURL+"/tarts")
+        if (this.tarts == undefined){
+            this.tarts = this.http.get<CakeInformation[]>(this.apiURL+"/tarts")
+        }
+        return this.tarts
     }
 
     public get_others(): Observable<any>{
-        return this.http.get<CakeInformation[]>(this.apiURL+"/others")
+        if (this.others == undefined){
+            this.others = this.http.get<CakeInformation[]>(this.apiURL+"/others")
+        }
+        return this.others;
     }
 
     public get_allproducts(): Observable<CakeInformation[]>{
-        return this.http.get<CakeInformation[]>(this.apiURL+"/allcakes")
+        if (this.all == undefined){
+            this.all = this.http.get<CakeInformation[]>(this.apiURL+"/allcakes")
+        }
+        return this.all;
     }
 
     public get_cakebyid(productId: string): Observable<CakeInformation>{
         return this.http.get<CakeInformation>(this.apiURL+"/cakes/"+productId)
+    }
+
+    public post_order(orderInformation: OrderInformation){
+        this.http.post(this.apiURL+"/order", orderInformation);
     }
 }
 
